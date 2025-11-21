5dfed5b5cf7621d3237c1e5c337a1ce2c397024a
shrushtipojage606-dot <shrushtipojage606@gmail.com>
2025-11-21 22:03:43 +0530

    Add initial project files  Upload main.py, musicLibrary.py, README.md, requirements.txt  Add source code and documentation
    Uploaded Python files for the Case voice assistant project. Includes all code, documentation, and dependency information.
    This commit sets up the basic project structure and Python requirements for development.
    No environment files are included as recommended.

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "lover": "https://youtu.be/b5Zay_Hd_7Q?list=RDb5Zay_Hd_7Q",
+    "bin tere": "https://youtu.be/JPZxMhZ4KDw?list=RDJPZxMhZ4KDw",
+    "jugrafira": "https://youtu.be/gRRMSF0nB0c?list=RDgRRMSF0nB0c",
+    "shaky": "https://youtu.be/sUf2PtEZris?list=RDsUf2PtEZris",
+    "chuttumale": "https://youtu.be/5vsOv_bcnhs?list=RD5vsOv_bcnhs"
+
+}
